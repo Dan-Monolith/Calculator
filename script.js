68a653f21fc4645db0593c142f395501ac20bb22
Dan Monolith <monolithicmail@gmail.com>
2023-08-01 13:55:09 +0500

    logic

--- script.js
+++ script.js
@@ -8,34 +8,29 @@ const currentOperandTextElement = document.querySelector('[data-current-operand]
 
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
-        this.previousOperandTextElement = previousOperandTextElement
-        this.currentOperandTextElement = currentOperandTextElement
-        this.clear()
-    }
-}
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    };
+};
 
-clear () {
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+clear() {
     this.currentOperand = ''
     this.previousOperand = ''
     this.operation = undefined
 }
 
-delete() {
-
-}
-
 appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+};
 
-}
-
-chooseOperation(operation) {
-
-}
-
-compute () {
-
-}
-
-updateDisplay() {
-
-}
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+        console.log(innerText);
+    })
+});
